1e5c27232c593b3c1cac03aeefab8fdee52c12db
AllenEsteves <estevesallen@yahoo.com>
2019-10-24 19:24:22 +0800

    fifth commit

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+
+    $('.top-nav . nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('.menu-toggler').on('click', function () {
+        $('html, body').animate(keyframes: {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options: 2000);
+    });
+
 });
