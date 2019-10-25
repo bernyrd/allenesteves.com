f3bc97255523ba3899f5a222fff11aacae8a4f00
AllenEsteves <estevesallen@yahoo.com>
2019-10-25 22:18:55 +0800

    9th commit

--- main.js
+++ main.js
@@ -4,28 +4,16 @@ $(document).ready(function () {
         $('.top-nav').toggleClass('open');
     });
 
-    $('.top-nav . nav-link').on('click', function () {
+    $('.top-nav , nav-link').on('click', function () {
         $('.menu-toggler').removeClass('open');
         $('.top-nav').removeClass('open');
     });
 
-    $('nav a[href*="#"]').on('click', function () {
-        $('html, body').animate(keyframes: {
-            scrollTop: $($(this).attr('href')).offset().top - 100
-        }, options: 2000);
-    });
-
-    $('#up').on('click', function () {
-        $('html, body').animate(keyframes: {
-            scrollTop: 0
-        }, options: 2000);
-    });
-
-
+    
+    
     AOS.init({
         easing: 'ease',
         duration: 1800,
-        once: true
     });
     
 });
